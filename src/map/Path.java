5e07b055cb96bc1341326ffdc3db800a3efaa984
danielnassauer <daniel.nassauer@mni.thm.de>
2015-06-27 11:38:23 +0200

    added weight

--- src/map/Path.java
+++ src/map/Path.java
@@ -4,13 +4,15 @@ public class Path {
 	public City city1, city2;
 	public double pheromones;
 	public double attractiveness;
+	public double weight;
 	public double length;
 
 	public Path(City city1, City city2) {
 		this.city1 = city1;
 		this.city2 = city2;
 		pheromones = 0;
-		attractiveness = 0;
+		attractiveness = 1 / length;
+		weight = 0;
 		length = getDistance(city1, city2);
 	}
 
